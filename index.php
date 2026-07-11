a6adf456bce8a1c849ab86aca50a6561747ea501
Teesha Matarage <tmatarage01@gmail.com>
2026-07-11 19:59:26 +0530

    Modified the index.php file

---
+++ index.php
@@ -0,0 +1,18 @@
+<html>
+    <head>
+        <title>Edge.Express</title>
+    </head>
+    <body>
+        <div class="header">
+            <img src="EE logo.png">
+            <h3>ආයුබෝවන් | WELCOME</h3>
+            <h1>Edge.Express</h1>
+            <h4>Order. Collect. Enjoy.</h4>
+        </div>
+        <div class="btn_section">
+            <a href="login.php">
+                <button class="get_started">Get Started</button>
+            </a>
+        </div>
+    </body>
+</html>
