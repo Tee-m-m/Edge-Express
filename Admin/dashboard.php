71c20fc8475ae6ba374abe4f5f00f7ed81cc34ce
Teesha Matarage <tmatarage01@gmail.com>
2026-07-11 21:23:58 +0530

    Developed the admin dashboard

---
+++ Admin/dashboard.php
@@ -0,0 +1,42 @@
+<?php
+
+session_start();
+
+if (!isset($_SESSION["admin_id"])) {
+    header("Location: login.php");
+    exit;
+}
+?>
+
+<html>
+    <head>
+        <title>Admin Dashboard</title>
+    </head>
+    <body>
+        <div class="admin-header">
+            <img src="EE logo.png">
+            <h1>ආයුබෝවන් | WELCOME</h1>
+            <h1>Admin Dashboard</h1>
+            <h1><?php echo $_SESSION["admin_username"]; ?>!</h1>
+            <?php
+            if(isset($_GET['login']) && $_GET['login'] == 'success') { ?>
+            <script>
+                alert("Login successful! Welcome to the Admin Dashboard");
+            </script>
+            <?php } ?>
+            <a href="logout.php">
+                <button>Logout</button>
+            </a>
+        </div>
+        <div class="admin_btn_section">
+            <a href="manage_food.php">
+                <button><h2>Manage Food</h2><br>
+                    <p>Add, Edit or Delete items here!</p></button>
+            </a>
+            <a href="manage_orders.php">
+                <button><h2>Manage Orders</h2><br>
+                    <p>Manage customer orders here!</p></button>
+            </a>
+        </div>
+    </body>
+</html>
--- index.php
+++ index.php
@@ -3,13 +3,13 @@
         <title>Edge.Express</title>
     </head>
     <body>
-        <div class="header">
+        <div class="main-header">
             <img src="EE logo.png">
             <h3>ආයුබෝවන් | WELCOME</h3>
             <h1>Edge.Express</h1>
             <h4>Order. Collect. Enjoy.</h4>
         </div>
-        <div class="btn_section">
+        <div class="main_btn_section">
             <a href="login.php">
                 <button class="get_started">Get Started</button>
             </a>
